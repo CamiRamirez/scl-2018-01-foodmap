8fcfca1cf9b02d4ba5de913402ad35a57cab1254
camila <camiramirezpp@gmail.com>
2018-07-25 11:16:32 -0400

    pantalla de inicio se esconde a los 2 sgs

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//Esconde la pantalla de inicio a los 2 segundos y muestra el resto de la App
+setTimeout(function hide() { $('#pantallaInicial').hide('fast');
+document.getElementById('pantallaApp').style.display = 'block'; }, 2000);
+
+//
+
+
+
+function onSearch(){
+
+}
+
+
+//key api zomato c7ca6ecef7e99e1d644aa2d5fab920d0
