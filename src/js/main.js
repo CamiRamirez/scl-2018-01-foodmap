8f4d71edb1f5a9b1efc8663adf53292a7e4b1f36
camila <camiramirezpp@gmail.com>
2018-07-26 11:06:05 -0400

    input con busqueda de locales

--- src/js/main.js
+++ src/js/main.js
@@ -9,24 +9,75 @@ var service;
 var infowindow;
 
 function initMap() {
-    // Locacion de la ciudad (Santiago)
     var city = { lat: -33.4569, lng: -70.648 };
-    
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 15, center: city });
-    // Marcador de ubicacion
     var marker = new google.maps.Marker({ position: city, map: map });
-}
-
-var map, infoWindow;
-function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -33.4569, lng: -70.648 },
-        zoom: 15
+        zoom: 15 //cercanía de imagen al mapa, así se alcanzan a ver calles y nombres.
+        
     });
     infoWindow = new google.maps.InfoWindow;
+    
 
-    // Geolocalizacion
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+   
+
+    // Muestra los resultados en la vista actual del mapa
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // detalles de cada lugar
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // Se obtiene el icono, el nombre y la ubicacion para cada lugar
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Se crear el marcador para cada lugar
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+
+    // GEOLOCALIZACION
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
@@ -56,7 +107,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 
-
 function initialize() {
   var pyrmont = new google.maps.LatLng(-33.4569,-70.648);
   map = new google.maps.Map(document.getElementById('map'), {
@@ -67,12 +117,14 @@ function initialize() {
   var request = {
     location: pyrmont,
     radius: '500',
-    type: ['restaurant']
+    type: ['restaurant', 'restoran']
   };
 
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 }
+
+
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -83,7 +135,23 @@ function callback(results, status) {
   }
 }
 
-// Funcion para busqueda por input
+
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap'
+    });
+    // Create the search box and link it to the UI element.
+    
+  }
+
+
+
+
+
+
+// Funcion para busqueda por input con api de restaurantes.
 function onSearch(){
 
 }
